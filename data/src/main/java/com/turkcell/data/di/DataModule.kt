bbc1898536a91723f5047da3910d2f076c304375
Ataberk Kaya <190316036@ogr.cbu.edu.tr>
2026-05-19 15:51:27 +0300

    .

--- data/src/main/java/com/turkcell/data/di/DataModule.kt
+++ data/src/main/java/com/turkcell/data/di/DataModule.kt
@@ -1,17 +1,25 @@
 package com.turkcell.data.di
 
 import com.turkcell.core.domain.AuthRepository
+import com.turkcell.data.local.TokenStore
+import com.turkcell.data.network.AuthInterceptor
+import com.turkcell.data.network.TokenAuthenticator
 import com.turkcell.data.remote.AuthApi
 import com.turkcell.data.repository.AuthRepositoryImpl
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.kotlinx.serialization.asConverterFactory
 
 private const val BASE_URL = "https://tickets-api.halitkalayci.com/"
+
+private val REFRESH_CLIENT = named("refresh_client")
+private val REFRESH_RETROFIT = named("refresh_retrofit")
+private val REFRESH_API = named("refresh_api")
 
 val dataModule = module {
     // Scope (Kapsam)
@@ -34,9 +42,45 @@ val dataModule = module {
         }
     }
 
+    single {
+        TokenStore(context=get())
+    }
+
+    single { AuthInterceptor(tokenStore = get()) }
+
+    single {
+        TokenAuthenticator(
+            tokenStore = get(),
+            refreshApiProvider = { get<AuthApi>(REFRESH_API) }
+        )
+    }
+
+    // Refresh Stack
+    single(REFRESH_CLIENT) {
+        OkHttpClient.Builder().addInterceptor(get<HttpLoggingInterceptor>()).build()
+    }
+
+    single(REFRESH_RETROFIT)
+    {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(get(REFRESH_CLIENT))
+            .addConverterFactory(get<Json>().asConverterFactory("application/json".toMediaType()))
+            .build()
+    }
+
+    single(REFRESH_API)
+    {
+        get<Retrofit>(REFRESH_RETROFIT).create(AuthApi::class.java)
+    }
+    // Refresh Stack
+
+
     // HTTP isteklerini yönetmek..
     single {
         OkHttpClient.Builder()
+            .addInterceptor(get<AuthInterceptor>())
+            .authenticator(get<TokenAuthenticator>())
             .addInterceptor(get<HttpLoggingInterceptor>())
             .build()
     }
@@ -48,12 +92,15 @@ val dataModule = module {
             .addConverterFactory(get<Json>().asConverterFactory("application/json".toMediaType()))
             .build()
     }
+    single {
+        get<Retrofit>().create(AuthApi::class.java)
+    }
 
-    single { get<Retrofit>().create(AuthApi::class.java) }
 
     single<AuthRepository> {
         AuthRepositoryImpl(
-            authApi = get()
+            authApi = get(),
+            tokenStore = get()
         )
     }
 
